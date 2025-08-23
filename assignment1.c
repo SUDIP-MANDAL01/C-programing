71bbb16a70c08cac29e838fcc7274a7aac55f073
Sudip Mandal <sudip.mandal5370@gmail.com>
2025-08-23 14:40:25 +0530

    Add files via upload

---
+++ assignment1.c
@@ -0,0 +1,40 @@
+#include<stdio.h>
+  int main(){
+
+float a,b,x;
+
+printf("Enter a:\n");
+scanf ("%f", &a);
+
+printf ("Enter b:\n");
+scanf("%f", &b);
+
+x=a+b;
+printf("sum of a&b is:%f",x);
+
+    return 0;
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
