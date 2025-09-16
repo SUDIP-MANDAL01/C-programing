c1dfe54393b6e0ca1e851b3cca3eb2dbcf5ce837
Sudip Mandal <sudip.mandal5370@gmail.com>
2025-09-16 15:58:08 +0530

    Assignment31.c

---
+++ Assignment31.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main (){
+
+int n , i;
+printf ("Enter an integer to print its multiplication table\n");
+scanf ("%d", &n);
+ 
+ printf ("Multiplication table of %d is:\n", n);
+    for (i=1; i<=10; i++){
+        printf ("%d * %d = %d\n", n, i, n*i);
+    }
+
+    return 0;
+}
